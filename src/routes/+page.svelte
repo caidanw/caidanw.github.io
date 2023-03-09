3cb71ce158bbc47cc5f6fb06880db53299f7d772
Caidan Williams <caidanstevenwilliams@gmail.com>
2023-03-08 18:18:13 -0800

    Update section titles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -53,25 +53,41 @@
 	</header>
 
 	<section class="space-y-4">
-		<p class="text-2xl">My Coding Activity & Stats</p>
+		<p class="text-3xl font-semibold">My Coding Activity & Stats</p>
 
 		<!-- Summary widget -->
 		<codersrank-summary {username} branding="false" show-avatar="false" min-width="640" />
 
 		<!-- Activity widget -->
-		<codersrank-activity {username} branding="false" labels legend tooltip step="3" svg-width="800" />
+		<codersrank-activity
+			{username}
+			branding="false"
+			labels
+			legend
+			tooltip
+			step="3"
+			svg-width="800"
+		/>
 
 		<!-- Skills chart widget -->
-		<codersrank-skills-chart {username} branding="false" labels legend tooltip svg-width="1200" svg-height="400"/>
+		<codersrank-skills-chart
+			{username}
+			branding="false"
+			labels
+			legend
+			tooltip
+			svg-width="1200"
+			svg-height="400"
+		/>
 	</section>
 	<section class="space-y-4">
-		<p class="text-2xl">Recent Projects</p>
+		<p class="text-3xl font-semibold">Recent Projects</p>
 
 		<!-- Portfolio widget -->
-		<codersrank-portfolio {username} branding="false" logos></codersrank-portfolio>
+		<codersrank-portfolio {username} branding="false" logos />
 	</section>
 	<section class="space-y-4">
-		<p class="text-2xl">Work Experience</p>
+		<p class="text-3xl font-semibold">Work Experience</p>
 
 		<!-- Work experience widget -->
 		<codersrank-work-experience {username} branding="false" logos />
