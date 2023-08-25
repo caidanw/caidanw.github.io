32e0a45431543054ae410c29d1c01a1a503dd3e0
Caidan Williams <caidanstevenwilliams@gmail.com>
2023-08-25 16:27:01 -0700

    feat: configure inter font with tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,12 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,ts,svelte}'],
   theme: {
+    fontFamily: {
+      sans: ['Inter Variable', ...defaultTheme.fontFamily.sans]
+    },
     extend: {},
   },
   plugins: [],
