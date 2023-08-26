12229019fd4eae02610b43db4b68d8e716dba3f5
Caidan Williams <caidanstevenwilliams@gmail.com>
2023-08-26 01:58:46 -0700

    feat: update tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,43 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultColors = require('tailwindcss/colors');
+
+console.log(defaultColors);
+
+// const colors = {
+// 	surface: defaultColors.zinc,
+
+// 	get light() {
+// 		return this.surface[50];
+// 	},
+// 	get dark() {
+// 		return this.surface[900];
+//   }
+// };
+
+let colors = {
+	// Source color palletes
+	surface: defaultColors.zinc
+};
+
+// Semantic color tokens from the pallete
+colors = {
+	light: colors.surface[50],
+	dark: colors.surface[900],
+	...colors
+};
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{html,js,ts,svelte}'],
-  theme: {
-    fontFamily: {
-      sans: ['Inter Variable', ...defaultTheme.fontFamily.sans]
-    },
-    extend: {
-      colors: {
-        surface: defaultTheme.colors.zinc
-      }
-    },
-  },
-  plugins: [],
-}
+const config = {
+	content: ['./src/**/*.{html,js,ts,svelte}'],
+	theme: {
+		fontFamily: {
+			sans: ['Inter Variable', ...defaultTheme.fontFamily.sans]
+		},
+		extend: {
+			colors
+		}
+	},
+	plugins: []
+};
+
+module.exports = config;
