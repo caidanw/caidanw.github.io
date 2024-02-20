88754987013442c7c35120b07ca01626d754804f
Caidan Williams <caidanstevenwilliams@gmail.com>
2024-02-19 18:21:50 -0800

    chore: wipe the repo and set it up using svelte 5

--- src/routes/old/footer.svelte
+++
@@ -1,5 +0,0 @@
-<script>
-	const currentYear = new Date().getFullYear();
-</script>
-
-<footer class="text-center opacity-60">Copyright &copy; {currentYear} Caidan Williams</footer>
--- svelte.config.js
+++ svelte.config.js
@@ -1,26 +1,16 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import { mdsvex } from 'mdsvex';
-
-/** @type {import('mdsvex').MdsvexOptions} */
-const mdsvexOptions = {
-	extensions: ['.md'],
-};
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	extensions: ['.svelte', '.md'],
-	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
 	kit: {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null,
-			precompress: false,
-			strict: true,
-		}),
-	},
+		adapter: adapter()
+	}
 };
 
 export default config;
