dfb7bd98d94e26cb1944cd14ec9ebb40c552c1e2
Caidan Williams <caidanstevenwilliams@gmail.com>
2024-02-19 19:27:03 -0800

    feat: add tailwindcss

---
+++ tailwind.config.cjs
@@ -0,0 +1,34 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultColors = require('tailwindcss/colors');
+
+let colors = {
+	// Source color palletes
+	surface: defaultColors.zinc
+};
+
+// Semantic color tokens from the pallete
+colors = {
+	light: colors.surface[50],
+	dark: colors.surface[900],
+	...colors
+};
+
+/** @type {import('tailwindcss').Config}*/
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		fontFamily: {
+			sans: ['Inter Variable', ...defaultTheme.fontFamily.sans],
+			serif: defaultTheme.fontFamily.serif,
+			mono: ['Source Code Pro Variable', ...defaultTheme.fontFamily.mono]
+		},
+		extend: {
+			colors
+		}
+	},
+
+	plugins: []
+};
+
+module.exports = config;
