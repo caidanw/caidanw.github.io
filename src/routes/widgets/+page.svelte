9acd809d045a04060685836de3907bd8df63c73b
Caidan Williams <caidanstevenwilliams@gmail.com>
2024-02-20 11:41:11 -0800

    feat: add LifeClock to widgets page

--- src/routes/widgets/+page.svelte
+++ src/routes/widgets/+page.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
+	import LifeClock from '$lib/components/lifeClock.svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
 </script>
+
+<LifeClock></LifeClock>
