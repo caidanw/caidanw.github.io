fed47d0df629ac0413ff8a5485c4c826cf95432f
Caidan Williams <caidanstevenwilliams@gmail.com>
2024-03-23 19:06:22 -0700

    feat: add info file

---
+++ src/lib/info.ts
@@ -0,0 +1,21 @@
+import AvatarImg from '$lib/images/avatar.png';
+
+// URL of the website
+export const website = 'https://caidan.dev';
+
+// Info about me
+export const firstName = 'Caidan';
+export const lastName = 'Williams';
+export const name = `${firstName} ${lastName}`;
+
+// Used for the landing page & footer of posts
+export const avatar = AvatarImg;
+
+// Social usernames, empty strings won't be used.
+export const github = 'mattjennings/sveltekit-blog-template';
+export const twitter = '';
+export const linkedin = '';
+export const instagram = '';
+
+// Links to other relevant things
+export const resume = '';
