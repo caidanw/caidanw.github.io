850589186378b4abef5610e3eb8ea6863697666a
Caidan Williams <caidanstevenwilliams@gmail.com>
2024-03-28 13:59:57 -0700

    update: layout to look a bit nicer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,8 @@
 
 	import { page } from '$app/stores';
 	import HolyGrailLayout from '$lib/components/HolyGrailLayout.svelte';
+	import SocialLinks from '$lib/components/SocialLinks.svelte';
+	import { resume, shortname } from '$lib/info';
 	import { cn } from '$lib/utils';
 
 	const siteRoutes = [
@@ -10,15 +12,7 @@
 		{ name: '/blog', url: '/blog' },
 		// { name: '/projects', url: '/projects' },
 		// { name: '/widgets', url: '/widgets' },
-		{
-			name: '/resume',
-			url: 'https://caidan-my.sharepoint.com/:b:/g/personal/me_caidan_dev/EcMqP0jC_VtIttsG8Q58-uYBN_jnnl_sDmbWQpQ4daPRTQ'
-		}
-	];
-
-	const externalRoutes = [
-		{ name: 'GitHub', url: 'https://github.com/caidanw' },
-		{ name: 'LinkedIn', url: 'https://www.linkedin.com/in/caidan-williams/' }
+		{ name: '/resume', url: resume }
 	];
 </script>
 
@@ -27,8 +21,8 @@
 	<div
 		slot="header"
 		class={cn(
-			'-mx-3 mb-4 flex flex-wrap items-start justify-between gap-2 px-3 py-3 md:mb-8',
-			'rounded-2xl border-y-[3px] border-dashed border-light/50 bg-dark/50 backdrop-blur'
+			'-mx-3 mb-4 flex flex-wrap items-center justify-between gap-2 px-3 py-3 md:mb-8',
+			'rounded-2xl border-y-[2px] border-dashed border-light/50 bg-dark/50 backdrop-blur'
 		)}
 	>
 		{#snippet dot(delay)}
@@ -39,19 +33,20 @@
 		{/snippet}
 
 		<span class="group inline-flex whitespace-nowrap font-mono text-2xl leading-7">
-			<span>Caidan W</span>
+			<span>{shortname}</span>
 			<i class="animate-pulse group-hover:animate-none">.</i>
 			{@render dot(150)}
 			{@render dot(300)}
 		</span>
 
-		<nav class="font-mono text-lg font-bold">
-			<ul class="flex flex-wrap justify-end gap-2">
+		<nav class="font-mono text-lg font-semibold">
+			<ul class="flex flex-wrap justify-end gap-1.5">
 				{#each siteRoutes as { name, url }}
 					<li
 						class={cn(
-							'rounded px-1.5 transition-colors hover:text-emerald-500',
-							url === $page.url.pathname && 'bg-light/10 text-emerald-500'
+							'rounded px-1.5 transition hover:text-accent-600',
+							url === $page.url.pathname && 'bg-light/10 text-accent-600',
+							name === '/resume' && 'pr-0'
 						)}
 					>
 						<a href={url}>{name}</a>
@@ -69,16 +64,10 @@
 	<!-- Footer -->
 	<div
 		slot="footer"
-		class="mt-6 flex items-center justify-between gap-8 border-t-[3px] border-dashed border-light/50 bg-dark pt-3"
+		class="mt-12 flex items-center justify-between gap-8 border-t-[2px] border-dashed border-light/50 pt-3"
 	>
-		<nav class="font-mono text-lg font-semibold">
-			<ul class="flex gap-4">
-				{#each externalRoutes as { name, url }}
-					<li class="rounded px-1.5 transition-colors hover:text-emerald-500">
-						<a href={url} rel="me">{name}</a>
-					</li>
-				{/each}
-			</ul>
+		<nav class="flex grow items-center justify-end gap-3">
+			<SocialLinks />
 		</nav>
 	</div>
 </HolyGrailLayout>
