cddd4995e6fa1d4480453c23b62b557090edcfad
Caidan Williams <caidanstevenwilliams@gmail.com>
2024-03-28 13:42:56 -0700

    update: info file with correct links

--- src/lib/info.ts
+++ src/lib/info.ts
@@ -6,16 +6,17 @@ export const website = 'https://caidan.dev';
 // Info about me
 export const firstName = 'Caidan';
 export const lastName = 'Williams';
-export const name = `${firstName} ${lastName}`;
+export const fullname = `${firstName} ${lastName}`;
+export const shortname = `${firstName} ${lastName[0]}`;
 
 // Used for the landing page & footer of posts
 export const avatar = AvatarImg;
 
 // Social usernames, empty strings won't be used.
-export const github = 'mattjennings/sveltekit-blog-template';
-export const twitter = '';
-export const linkedin = '';
-export const instagram = '';
+export const github = 'https://github.com/caidanw';
+export const twitter = 'https://twitter.com/caidanwilliams';
+export const linkedin = 'https://www.linkedin.com/in/caidan-williams/';
 
 // Links to other relevant things
-export const resume = '';
+export const resume =
+	'https://caidan-my.sharepoint.com/:b:/g/personal/me_caidan_dev/EcMqP0jC_VtIttsG8Q58-uYBN_jnnl_sDmbWQpQ4daPRTQ';
