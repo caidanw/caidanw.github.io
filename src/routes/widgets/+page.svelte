ae7cff48430e733e53a1fe0cf7836dc3350db674
Caidan Williams <caidanstevenwilliams@gmail.com>
2025-11-13 21:11:14 -0800

    chore: delete old files

--- src/routes/widgets/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import LifeClock from '$lib/components/lifeClock.svelte';
-	import type { PageData } from './$types';
-
-	export let data: PageData;
-</script>
-
-<LifeClock></LifeClock>
