f658b8004e692d36a6f1491fb85c2f81d91717ce
Caidan Williams <caidanstevenwilliams@gmail.com>
2025-12-09 15:01:00 -0800

    refactor(lab): extract debounce utility to src/lib/utils for reuse

---
+++ src/lib/utils.ts
@@ -0,0 +1,11 @@
+function debounce(func: (...args: any[]) => void, wait: number) {
+  let timeout: ReturnType<typeof setTimeout> | undefined
+  return function executedFunction(...args: any[]) {
+    const later = () => {
+      clearTimeout(timeout)
+      func(...args)
+    }
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+  }
+}
